c479600a62b016e106cd38a5ad597443541b64f9
is0692vs <is0692vs@ed.ritsumei.ac.jp>
2025-10-25 23:58:10 +0900

    release: v1.0.1 - publisher を hirokimukai に変更

--- src/test/extension.test.ts
+++ src/test/extension.test.ts
@@ -5,11 +5,11 @@ suite('Extension Test Suite', () => {
 	vscode.window.showInformationMessage('Start all tests.');
 
 	test('Extension should be present', () => {
-		assert.ok(vscode.extensions.getExtension('is0692vs.code-mantra'));
+		assert.ok(vscode.extensions.getExtension('hirokimukai.code-mantra'));
 	});
 
 	test('Should activate extension', async () => {
-		const ext = vscode.extensions.getExtension('is0692vs.code-mantra');
+		const ext = vscode.extensions.getExtension('hirokimukai.code-mantra');
 		await ext?.activate();
 		assert.ok(ext?.isActive);
 	});
